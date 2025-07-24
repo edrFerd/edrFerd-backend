ee8f4595087cd7c147e88edf1f6b0a38b961b0a4
erduotong <erduotong@qq.com>
2025-07-24 22:05:37 +0800

    refactor: make World struct and GLOBAL_WORLD static variable public

--- src/libs/world.rs
+++ src/libs/world.rs
@@ -4,10 +4,10 @@ use foldhash::HashMapExt;
 use std::sync::OnceLock;
 
 type WorldMapType = foldhash::HashMap<BlockPoint, BlockInfo>;
-struct World {
+pub struct World {
     world: WorldMapType,
 }
-static GLOBAL_WORLD: OnceLock<World> = OnceLock::new();
+pub static GLOBAL_WORLD: OnceLock<World> = OnceLock::new();
 impl World {
     pub fn new() -> Self {
         Self {
@@ -16,5 +16,5 @@ impl World {
     }
 }
 pub fn get_world() -> &'static World {
-    GLOBAL_WORLD.get_or_init( World::new)
+    GLOBAL_WORLD.get_or_init(World::new)
 }
