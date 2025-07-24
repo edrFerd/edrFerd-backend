76aea78cc6d6c5ae0af2562c9af3567ed49cd8da
shenjack-5600u <3695888@qq.com>
2025-07-24 14:02:47 +0800

    啊啊啊啊啊
    Co-authored-by: erduotong <erduotong@qq.com>

--- src/data_struct.rs
+++ src/data_struct.rs
@@ -1,16 +1,29 @@
+use chrono::NaiveTime;
+
+/// 一个块
 pub struct Chunk {
     version: String,
     prev_hash: String,
     explanation: Vec<Block>,
-    timestamp: u128,
-    pubkey: String,
-    pow: String,
+    timestamp: NaiveTime,
+    pubkey: PubKey,
+    pow: Pow,
     sign: String,
 }
+
+pub struct Pow {
+
+}
+
+pub struct PubKey {
+    key: String
+}
+
 pub struct Block {
     point: Point,
     block_appearance: BlockAppearance,
 }
+
 pub struct Point {
     x: i64,
     y: i64,
