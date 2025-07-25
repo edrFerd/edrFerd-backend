1e4a249db83ad69edfcda7cbb2d266453021b5a7
erduotong <erduotong@qq.com>
2025-07-25 21:56:06 +0800

    refactor: clean up App.vue by removing unused imports and simplifying error handling

--- test_frontend/src/App.vue
+++ test_frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
+import {ref} from "vue";
 
 const message = ref('')
 
@@ -11,8 +10,7 @@ async function callTestSend() {
     message.value = data
     console.log(data)
   } catch (error) {
-    const errorMsg = `Error: ${error.message}`;
-    message.value = errorMsg
+    message.value = `Error: ${error.message}`
     console.error('Error calling test_send:', error)
   }
 }
@@ -20,32 +18,11 @@ async function callTestSend() {
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-
-  <div>
+    <h1>EdrFerd 测试</h1>
     <button @click="callTestSend">发送共识包</button>
     <p v-if="message">服务器返回: {{ message }}</p>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
